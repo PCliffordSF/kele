3e3dfc920d534cb46d2e31a45890a12a64704960
Paul Clifford <paulclifford2570@gmail.com>
2017-04-26 23:40:28 +0000

    finish chkp-4

--- lib/kele.rb
+++ lib/kele.rb
@@ -18,14 +18,29 @@ class Kele
         token = Kele.post(@sessions_url, options)
         hash_item = JSON.parse(token.body)
         @auth_token = hash_item["auth_token"]
+        @user = hash_item["user"]
         self
     end
     
     def self.get_me
-        @user_url = @base_api_url + 'users/me'
-        @response = Kele.get(@user_url, headers: { "authorization" => @auth_token })
-        @me = JSON.parse(@response.body)
-        @me
+        user_url = @base_api_url + 'users/me'
+        response = Kele.get(user_url, headers: { "authorization" => @auth_token })
+        JSON.parse(response.body)
+    end
+    
+    def self.get_mentor_id
+        user_url = @base_api_url + 'users/me'
+        response = Kele.get(user_url, headers: { "authorization" => @auth_token })
+        JSON.parse(response.body)["current_enrollment"]["mentor_id"]
+    end
+    
+    
+    def self.get_mentor_availability mentor_id
+       mentor_availability_url = @base_api_url + 'mentors/'
+       mentor_availability_url += mentor_id.to_s
+       mentor_availability_url += '/student_availability'
+       response = Kele.get(mentor_availability_url, headers: { "authorization" => @auth_token })
+       JSON.parse(response.body)
     end
     
   def self.hi
